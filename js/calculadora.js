cd0a000221aed13cc9a7b6ea983d3dca26a259e1
eduardobarros13 <eduardobarros.eng@outlook.com>
2021-05-14 19:12:45 -0300

    Ajustes nos precos de pagamento

--- js/calculadora.js
+++ js/calculadora.js
@@ -14,7 +14,13 @@ function calculo() {
 
 	document.meu_form.comissaoBruta.value = comissaoBruta.toFixed(2);
 
-	var gateway = 0.0385 // Taxa de pagamento 3.85%
+
+	var gateway = Number(document.meu_form.gateway.value);
+
+	gateway = (gateway / 100);
+
+
+	//var gateway = 0.0385 // Taxa de pagamento 3.85%
 	var pis = 0.0165 // Imposto PIS 1.65%
 	var confins = 0.076 // Imposto Confins 7.60%
 	subtotal = comissaoBruta - ((comissaoBruta * gateway) + (comissaoBruta * pis) + (comissaoBruta * confins))
